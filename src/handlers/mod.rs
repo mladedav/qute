e84220393d8d3d0cd1bc5f40907b6d1b5a16dfe6
David Mládek <david.mladek.cz@gmail.com>
2023-09-20 23:43:48 +0200

    Remove dead code

--- src/handlers/mod.rs
+++ src/handlers/mod.rs
@@ -1,15 +1,3 @@
-use mqttbytes::v5::Packet;
-
 pub(super) mod connect;
 pub(super) mod publish;
 pub(super) mod subscribe;
-
-trait OutgoingMqttService<Request> {}
-
-trait IncomingHandler<Request> {
-    fn handle(packet: Request) -> Vec<Packet>;
-}
-
-trait OutgoingHandler<Request> {
-    fn handle(packet: &mut Request);
-}
